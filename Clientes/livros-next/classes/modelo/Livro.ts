9c9ba4022d0efe6a88e414ba34036bda21ed407d
ChicoFullStack <140274298+ChicoFullStack@users.noreply.github.com>
2024-02-29 19:38:11 -0300

    first commit

---
+++ Clientes/livros-next/classes/modelo/Livro.ts
@@ -0,0 +1,29 @@
+export class Livro {
+
+  codigo:string ='';
+  codEditora:number = 0;
+  titulo:string = '';
+  resumo:string = '';
+  autores:Array<string> = [];
+
+}
+
+
+// class Livro {
+//     _id: string;
+//     codEditora: number;
+//     titulo: string;
+//     resumo: string;
+//     autores: string[];
+  
+//     constructor(_id: string, codEditora: number, titulo: string, resumo: string, autores: string[]) {
+//       this._id = _id;
+//       this.codEditora = codEditora;
+//       this.titulo = titulo;
+//       this.resumo = resumo;
+//       this.autores = autores;
+//     }
+//   }
+  
+//   export default Livro;
+  
